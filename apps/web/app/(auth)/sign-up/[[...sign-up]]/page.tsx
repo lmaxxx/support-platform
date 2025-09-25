531ba542706f34d667d8248e6bd0ab31c6dde56d
Maksym Leshchenko <leshchenko.maksym10@gmail.com>
2025-09-25 17:18:53 +0200

    feat: connect clerk auth

---
+++ apps/web/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import {SignIn, SignUp} from "@clerk/nextjs";
+
+export default function Page () {
+  return (
+    <SignUp/>
+  );
+};
+
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,33 @@
 "use client"
 
-import {useMutation, useQuery} from "convex/react";
+import {Unauthenticated, useMutation, useQuery} from "convex/react";
 import {api} from "@workspace/backend/convex/_generated/api";
 import {Button} from "@workspace/ui/components/button";
+import {Authenticated} from "convex/react";
+import {SignInButton, UserButton} from "@clerk/nextjs";
 
 export default function Page() {
   const users = useQuery(api.users.getMany)
   const addUser = useMutation(api.users.add)
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World apps/web</h1>
-        <Button onClick={() => addUser()}>ADD</Button>
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">Hello World apps/web</h1>
+            <Button onClick={() => addUser()}>ADD</Button>
+            {JSON.stringify(users, null, 2)}
+          </div>
+          <UserButton/>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        You have to sign in
+        <SignInButton></SignInButton>
+      </Unauthenticated>
+    </>
+
+
   )
 }
