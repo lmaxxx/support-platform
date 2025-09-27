753c098b8b4fc37d73af0a2b1e837299388954cf
Maksym Leshchenko <leshchenko.maksym10@gmail.com>
2025-09-27 16:38:27 +0200

    feat: create dashboard layout for widget app

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,21 +1,19 @@
 "use client"
 
 import {Button} from "@workspace/ui/components/button";
-import {useVapi} from "@/modules/hooks/use-vapi";
+import WidgetView from "@/modules/ui/views/widget-view";
+import {use} from "react";
 
-export default function Page() {
-  const {startCall, endCall, transcript} = useVapi()
+type Props = {
+  searchParams: Promise<{
+    organizationId: string
+  }>
+}
+
+export default function Page({searchParams}: Props) {
+  const {organizationId} = use(searchParams);
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World apps/web</h1>
-        <Button onClick={() => startCall()}>Start call</Button>
-        <Button variant={"ghost"} onClick={() => endCall()}>End call</Button>
-        {/*{JSON.stringify(users, null, 2)}*/}
-
-        <p>{JSON.stringify(transcript, null, 2)}</p>
-      </div>
-    </div>
+    <WidgetView organizationId={organizationId}/>
   )
 }
