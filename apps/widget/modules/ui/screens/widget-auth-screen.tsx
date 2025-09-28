b0c5dc2330fbf5a4498c83b9367279a3228cf37e
Maksym Leshchenko <leshchenko.maksym10@gmail.com>
2025-09-28 13:59:05 +0200

    feat: create widget session functionality

---
+++ apps/widget/modules/ui/screens/widget-auth-screen.tsx
@@ -0,0 +1,118 @@
+import WidgetHeader from "@/modules/ui/components/widget-header";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormField, FormItem, FormMessage} from "@workspace/ui/components/form";
+import {Input} from "@workspace/ui/components/input";
+import {Button} from "@workspace/ui/components/button";
+import {useMutation} from "convex/react";
+import {api} from "@workspace/backend/convex/_generated/api";
+import {Doc} from "@workspace/backend/convex/_generated/dataModel";
+
+const formSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+})
+
+type FormType = z.infer<typeof formSchema>;
+
+const organizationId = "123" // for testing
+
+export default function WidgetAuthScreen() {
+  const form = useForm<FormType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+    }
+  })
+
+  const createContactSession = useMutation(api.public.contactSessions.create)
+
+  const onSubmit = async (values : FormType) => {
+    if(!organizationId) {
+      return;
+    }
+
+    const metadata: Doc<"contactSessions">["metadata"] = {
+      userAgent: navigator.userAgent,
+      language: navigator.language,
+      languages: navigator.languages?.join(","),
+      platform: navigator.platform,
+      vendor: navigator.vendor,
+      screenResolution: `${screen.width}x${screen.height}`,
+      viewportSize: `${window.innerWidth}x${window.innerHeight}`,
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      timezoneOffset: new Date().getTimezoneOffset(),
+      cookieEnabled: navigator.cookieEnabled,
+      referrer: document.referrer || "direct",
+      currentUrl: window.location.href,
+    }
+
+    const contactSessionId = await createContactSession({
+      ...values,
+      metadata,
+      organizationId
+    })
+  }
+
+
+  return (
+    <>
+      <WidgetHeader>
+        <div className={"flex flex-col justify-between gap-y-2 px-2 py-6"}>
+          <p className={"text-3xl"}>
+            Hi there! 👋
+          </p>
+          <p className={"text-lg"}>
+            Let&apos;s get you started
+          </p>
+        </div>
+      </WidgetHeader>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className={"flex flex-1 flex-col gap-y-4 p-4"}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className={"h-10 bg-background"}
+                    placeholder={"e.g. John Doe"}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    className={"h-10 bg-background"}
+                    placeholder={"e.g. John Doe@example.com"}
+                    type={"email"}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage/>
+              </FormItem>
+            )}
+          />
+          <Button
+            disabled={form.formState.isSubmitting}
+            size={"lg"}
+            type="submit"
+          >
+            Continue
+          </Button>
+        </form>
+      </Form>
+    </>
+  )
+}
